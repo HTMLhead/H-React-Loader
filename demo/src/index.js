2372aa66070bcc691731fc52baa8bddcf8f84d20
HTMLhead <mrdoo309@gmail.com>
2019-10-16 22:59:59 +0900

    update(demo): 데모 부분 추가

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 
 import { SpinnerFace } from "../../src/Spinner";
+import { DynamicRotate } from "../../src/DynamicSpinner";
 
 class Demo extends Component {
   render() {
     return (
       <div>
         <h1>h-react-loader Demo</h1>
+        <DynamicRotate />
         <SpinnerFace />
       </div>
     );
